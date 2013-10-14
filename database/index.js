71c5c64245b0d349ad5fe789bc40fdb6da46fbe3
David Lee <monodlee@gmail.com>
2013-10-14 14:59:44 -0700

    added redis cloud capability

--- database/index.js
+++ database/index.js
@@ -1,7 +1,10 @@
-redis = require('redis');
-db = redis.createClient();
+var redis = require('redis');
+//db = redis.createClient();
+var url = require('url');
+var redisURL = url.parse(process.env.REDISCLOUD_URL);
+var db = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+db.auth(redisURL.auth.split(":")[1]);
 module.exports.db = db;
-
 function userExists(user, cb){
 	cb = cb || function(){};
 	db.hget('users', user, function(err, reply){
